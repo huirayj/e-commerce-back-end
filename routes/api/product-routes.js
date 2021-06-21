3dcdb65529de7910fb17ba5891f889c646d68c61
Justin Hui <huirayj@gmail.com>
2021-06-21 19:33:11 -0400

    updated product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -133,7 +133,7 @@ router.delete('/:id', async (req, res) => {
     });
 
     if (productData) {
-      res.status(404).json({ message: 'No product found with this id.' });
+      res.status(404).json({ message: 'No product found with that id.' });
       return;
     }
 
