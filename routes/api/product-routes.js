3cd998ba64af41942776d9cf3369b30aefd6d830
Justin Hui <huirayj@gmail.com>
2021-06-22 16:28:31 -0400

    swapped back to findbypk

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -32,8 +32,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findOne({
-        where: { id: req.params.id },
+    const productData = await Product.findByPk(req.params.id, {
         include: [
           {
             model: Category,
