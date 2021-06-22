869a4545587012858f5fc12445a888c1723ba4d5
Justin Hui <huirayj@gmail.com>
2021-06-22 16:17:47 -0400

    changed back to category_id, added ondelete

--- models/index.js
+++ models/index.js
@@ -10,7 +10,8 @@ Product.belongsTo(Category, {
 });
 // Categories have many Products
 Category.hasMany(Product, { 
-  foreignKey: 'product_id' 
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE' 
 });
 
 // Products belongToMany Tags (through ProductTag)
