a30122b06219f363c4a6cb29fc6d9009c33e07d1
Justin Hui <huirayj@gmail.com>
2021-06-22 17:14:44 -0400

    fixed typo

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,7 +37,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const newCategoryData = await newCategoryData.create(req.body);
+    const newCategoryData = await Category.create(req.body);
 
     res.status(200).json(newCategoryData);
   } catch (err) {
